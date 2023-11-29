d0c534e77efeb5a265bad715d4834a0580d2cbba
Matthew <matt@sigouin.codes>
2023-11-29 14:58:43 -0500

    Update project demo and repo links

--- data/projects.ts
+++ data/projects.ts
@@ -11,19 +11,11 @@ export interface IProject {
 
 const projects: IProject[] = [
   {
-    id: "dashdoor",
-    name: "DashDoor",
-    img: "dashdoor.jpg",
-    description: 'A classic Dashdoor mockup that only has specifically Chick-fil-a as a store which only sells 4 items that you cannot really purchase.',
-    demoURL: 'http://dashdoor.sigouin.com',
-    gitURL: 'https://github.com/sigouin/dashdoor'
-  },
-  {
     id: "whackamole",
-    name: "whackamole",
+    name: "Whack-a-Mole",
     img: "whackamole.jpg",
     description: 'This is a simple and fun game where you have to quickly tap or click on moles that pop up from their holes. The goal is to whack as many moles as possible within a given time limit.',
-    demoURL: 'http://whackamole.sigouin.com',
+    demoURL: 'https://whackamole-navy.vercel.app/',
     gitURL: 'https://github.com/sigouin/whackamole'
   },
   {
@@ -31,15 +23,23 @@ const projects: IProject[] = [
     name: "package.json crawler",
     img: "package.json crawler.jpg",
     description: 'Crawling through your package.json and checking your deps.',
-    demoURL: 'http://packagejsoncrawler.sigouin.com',
-    gitURL: 'https://github.com/sigouin/packagejsoncrawler'
+    demoURL: 'https://github.com/Sigouin/crawler',
+    gitURL: 'https://github.com/Sigouin/crawler'
+  },
+  {
+    id: "dashdoor",
+    name: "DashDoor",
+    img: "dashdoor.jpg",
+    description: 'A classic DoorDash mockup that only has specifically Chick-fil-a as a store which only sells 4 items that you cannot really purchase.',
+    demoURL: 'https://dashdoor.vercel.app/',
+    gitURL: 'https://github.com/Sigouin/dashdoor'
   },
   {
     id: "ratingcomponent",
-    name: "rating component",
+    name: "Rating Component",
     img: "ratingcomp.png",
     description: 'This was a simple challenge hosted on Frontend Mentor. It is aimed to practice user interactions and updated the DOM.',
-    demoURL: 'http://ratingcomponent.sigouin.com',
+    demoURL: 'https://matts-ratingcomp-81c54f.netlify.app/',
     gitURL: 'https://github.com/sigouin/ratingcomponent'
   },
 ]
