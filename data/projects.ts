d034ad35eb4953ad38d466b595d48caa0453f846
Matthew <matt@sigouin.codes>
2024-06-10 18:39:32 -0400

    Updating projects

--- data/projects.ts
+++ data/projects.ts
@@ -11,12 +11,20 @@ export interface IProject {
 
 const projects: IProject[] = [
   {
-    id: "whackamole",
-    name: "Whack-a-Mole",
-    img: "whackamole.jpg",
-    description: 'This is a simple and fun game where you have to quickly tap or click on moles that pop up from their holes. The goal is to whack as many moles as possible within a given time limit.',
-    demoURL: 'https://whackamole-navy.vercel.app/',
-    gitURL: 'https://github.com/sigouin/whackamole'
+    id: "prolog",
+    name: "Prolog Error Tracking Tool",
+    img: "prolog.jpg",
+    description: 'This repository is part of the React Job Simulator where you work in a professional dev environment with advanced tooling and workflows.',
+    demoURL: 'https://prolog-app-sigouin.vercel.app/',
+    gitURL: 'https://github.com/profydev/prolog-app-Sigouin'
+  },
+  {
+    id: "marketplace",
+    name: "Online Marketplace",
+    img: "marketplace.jpg",
+    description: 'This is a simple online marketplace where you can add, edit, and delete products. It also has user messaging and authentication.',
+    demoURL: 'https://online-marketplace-one.vercel.app/',
+    gitURL: 'https://github.com/Sigouin/online-marketplace'
   },
   {
     id: "packagejsoncrawler",
@@ -30,18 +38,26 @@ const projects: IProject[] = [
     id: "dashdoor",
     name: "DashDoor",
     img: "dashdoor.jpg",
-    description: 'A classic DoorDash mockup that only has specifically Chick-fil-a as a store which only sells 4 items that you cannot really purchase.',
+    description: 'A classic DoorDash mockup that only has specifically Chick-fil-a as a store which only sells four items that you cannot really purchase.',
     demoURL: 'https://dashdoor.vercel.app/',
     gitURL: 'https://github.com/Sigouin/dashdoor'
   },
   {
-    id: "ratingcomponent",
-    name: "Rating Component",
-    img: "ratingcomp.png",
-    description: 'This was a simple challenge hosted on Frontend Mentor. It is aimed to practice user interactions and updated the DOM.',
-    demoURL: 'https://matts-ratingcomp-81c54f.netlify.app/',
-    gitURL: 'https://github.com/sigouin/ratingcomponent'
+    id: "whackamole",
+    name: "Whack-a-Mole",
+    img: "whackamole.jpg",
+    description: 'This is a simple and fun game where you have to quickly tap or click on moles that pop up from their holes. The goal is to whack as many moles as possible within a given time limit.',
+    demoURL: 'https://whackamole-navy.vercel.app/',
+    gitURL: 'https://github.com/sigouin/whackamole'
   },
+  // {
+  //   id: "ratingcomponent",
+  //   name: "Rating Component",
+  //   img: "ratingcomp.png",
+  //   description: 'This was a simple challenge hosted on Frontend Mentor. It is aimed to practice user interactions and updating the DOM.',
+  //   demoURL: 'https://matts-ratingcomp-81c54f.netlify.app/',
+  //   gitURL: 'https://github.com/sigouin/ratingcomponent'
+  // },
 ]
 
 export default projects
